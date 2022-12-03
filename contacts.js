a0afda69b92ffe0bcadd5b5e46a291fd25a043ab
OlyaNegrych <olyanegrych@gmail.com>
2022-12-03 14:02:57 +0200

    bug fix, seems like works

--- contacts.js
+++ contacts.js
@@ -1,48 +1,52 @@
 const fs = require("fs").promises;
 const path = require("path");
-// const { nanoid } = require("nanoid");
-import { nanoid } from "nanoid";
+const { uid } = require("uid");
 
-const contactsPath = path.resolve("./contacts.json");
+const contactsPath = path.resolve("db/contacts.json");
 
 async function listContacts() {
-    try {
-      const data = JSON.parse(await fs.readFile(contactsPath, "utf8"));
-      console.log(data);
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
-}
-
-async function getContactById(contactId) {
-    try {
-        const data = JSON.parse(await fs.readFile(contactsPath, "utf8"));
-        const contactById = data.find((contact) => contact.id === contactId);
-        return contactById;
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-async function removeContact(contactId) {
   try {
-      const data = JSON.parse(await fs.readFile(contactsPath, "utf8"));
-      const filteredContacts = JSON.stringify(data.filter(contact => contact.id !== contactId));
-      await fs.writeFile(contactsPath, filteredContacts, 'utf8');
+    const data = JSON.parse(await fs.readFile(contactsPath, "utf8"));
+    console.log(data);
+    return data;
   } catch (error) {
     console.log(error);
   }
 }
 
-async function addContact({name, email, phone}) {
- try {
+async function getContactById(contactId) {
+  try {
     const data = JSON.parse(await fs.readFile(contactsPath, "utf8"));
-     const newContact = JSON.stringify(data.push({ id: nanoid(3), name, email, phone }));
-     await fs.writeFile(contactsPath, newContact, "utf8");
- } catch (error) {
+    const contactById = data.find((contact) => contact.id === contactId);
+    console.log(contactById);
+    return contactById;
+  } catch (error) {
     console.log(error);
- }
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const data = JSON.parse(await fs.readFile(contactsPath, "utf8"));
+    const filteredContacts = JSON.stringify(
+      data.filter((contact) => contact.id !== contactId)
+    );
+    await fs.writeFile(contactsPath, filteredContacts, "utf8");
+    console.log("The contact was removed");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function addContact({ name, email, phone }) {
+  try {
+    const data = JSON.parse(await fs.readFile(contactsPath, "utf8"));
+    data.push({ id: uid(3), name, email, phone });
+    await fs.writeFile(contactsPath, JSON.stringify(data), "utf8");
+    console.log("The contact was added");
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = {
